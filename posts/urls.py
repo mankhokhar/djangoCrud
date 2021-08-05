e858fe1ddd1b2f80579330155983307191861764
Ahmad Naeem <mnaeem.bese17seecs@seecs.edu.pk>
2021-08-05 16:16:10 +0500

    Create Comment and Add React added

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    #/ /
-    path('', views.post_list_view, name='post_list')
+    # / /
+    path('', views.post_list_view, name='post_list'),
+    # /5/react
+    path('<int:post_id>/react', views.react, name='react'),
+    # /5/add_comment
+    path('<int:post_id>/add_comment', views.add_comment, name='add_comment')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+
 
 from .models import Post
 # Create your views here.
@@ -10,3 +10,14 @@ def post_list_view(request):
     return render(request, 'posts/post_list.html', context={'posts_list': posts})
 
 
+def react(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.reacts += 1
+    post.save()
+    return post_list_view(request)
+
+
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.comment_set.create(comment_text=request.POST['comment_text'])
+    return post_list_view(request)
